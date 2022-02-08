0309ec3a6d14b1d7839358c4d3cd6b8bcfac286f
hors <horsescary@gmail.com>
2022-02-08 08:32:35 -0500

    started work on telescope poster

---
+++ telescope/present.js
@@ -0,0 +1,8 @@
+function nextSlide() {
+    currentSlide = document.getElementById("currentSlide").getAttribute("slide")
+
+    document.getElementById(currentSlide).setAttribute("style", "display: none;")
+    document.getElementById(parseInt(currentSlide) + 1 ).setAttribute("style", "display: block;")
+
+    document.getElementById("currentSlide").setAttribute("slide", parseInt(currentSlide) + 1)
+}
