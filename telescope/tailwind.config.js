4b9d6323d9a397dc0d0eb120b4df906a42f9e69c
hors <horsescary@gmail.com>
2022-02-08 08:40:55 -0500

    made grey work as gray

--- telescope/tailwind.config.js
+++ telescope/tailwind.config.js
@@ -1,7 +1,12 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: ['*.{html,js}'],
   theme: {
     extend: {},
+    colors: {
+      grey: colors.gray,
+    }
   },
   plugins: [],
 }
