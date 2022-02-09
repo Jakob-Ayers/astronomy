c3ccc54f53db9d38404639c6b58dae6f0ac8348e
hors <horsescary@gmail.com>
2022-02-09 09:48:19 -0500

    telescope changes

--- telescope/present.js
+++ telescope/present.js
@@ -6,3 +6,12 @@ function nextSlide() {
 
     document.getElementById("currentSlide").setAttribute("slide", parseInt(currentSlide) + 1)
 }
+
+function slideBack() {
+    currentSlide = document.getElementById("currentSlide").getAttribute("slide")
+
+    document.getElementById(currentSlide).setAttribute("style", "display: none;")
+    document.getElementById(parseInt(currentSlide) - 1 ).setAttribute("style", "display: block;")
+
+    document.getElementById("currentSlide").setAttribute("slide", parseInt(currentSlide) - 1) 
+}
