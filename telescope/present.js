e14216bd55fea25df9cfc9a8e4c3376da0244a1b
hors <horsescary@gmail.com>
2022-02-17 09:41:46 -0500

    Telescope Update added more info to index made javascript detect last slide made javascript overall better

--- telescope/present.js
+++ telescope/present.js
@@ -1,17 +1,46 @@
 function nextSlide() {
-    currentSlide = document.getElementById("currentSlide").getAttribute("slide")
+    if (isLastSlide()) {
+        console.log("Last Slide!")
+    }
+    else {
+        currentSlide = parseInt(getCurrentSlide())
 
-    document.getElementById(currentSlide).setAttribute("style", "display: none;")
-    document.getElementById(parseInt(currentSlide) + 1 ).setAttribute("style", "display: block;")
+        document.getElementById(currentSlide).setAttribute("style", "display: none;")
+        document.getElementById(currentSlide + 1).setAttribute("style", "display: block;")
 
-    document.getElementById("currentSlide").setAttribute("slide", parseInt(currentSlide) + 1)
+        document.getElementById("slideInfo").setAttribute("currentslide", currentSlide + 1)
+    }
 }
 
 function slideBack() {
-    currentSlide = document.getElementById("currentSlide").getAttribute("slide")
+    currentSlide = parseInt(getCurrentSlide()) 
 
     document.getElementById(currentSlide).setAttribute("style", "display: none;")
-    document.getElementById(parseInt(currentSlide) - 1 ).setAttribute("style", "display: block;")
+    document.getElementById(currentSlide - 1).setAttribute("style", "display: block;")
 
-    document.getElementById("currentSlide").setAttribute("slide", parseInt(currentSlide) - 1) 
+    document.getElementById("slideInfo").setAttribute("currentslide", currentSlide - 1)
+}
+
+function getCurrentSlide() {
+    return (document.getElementById("slideInfo").getAttribute("currentslide"))
+}
+
+function isLastSlide() {
+    totalSlides = document.getElementById("slideInfo").getAttribute("totalslides")
+    currentSlide = parseInt(getCurrentSlide())
+    if (currentSlide == totalSlides) {
+        return (true)
+    }
+    else {
+        return (false)
+    }
+}
+
+function isFirstSlide() {
+    if (parseInt(getCurrentSlide()) == 0) {
+        return (true)
+    }
+    else {
+        return (false)
+    }
 }
